5a6a1ae3f21b1d39d623757f63a3a1bd50894303
tiffanyxmlim <tiffanyxmlim@gmail.com>
2014-12-03 18:15:36 -0500

    first commit

---
+++ DrinkCounter/Counter.swift
@@ -0,0 +1,35 @@
+//
+//  homepage.swift
+//  DrinkCounter
+//
+//  Created by Tiffany Lim on 11/30/14.
+//  Copyright (c) 2014 Tiffany Lim. All rights reserved.
+//
+
+import UIKit
+
+class homepage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DrinkCounter/Register.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController2.swift
+//  DrinkCounter
+//
+//  Created by Tiffany Lim on 11/30/14.
+//  Copyright (c) 2014 Tiffany Lim. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController2: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
