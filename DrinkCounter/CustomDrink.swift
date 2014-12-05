0584f1c568b85dd00124ad2f1aadd62a0ccdca3a
tiffanyxmlim <tiffanyxmlim@gmail.com>
2014-12-04 21:54:10 -0500

    sliders for custom value, fixed button on liquor

--- DrinkCounter/CustomDrink.swift
+++ DrinkCounter/CustomDrink.swift
@@ -13,124 +13,60 @@ class CustomDrink: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        sliderABV.value = 0.0
+        sliderVolume.value = 0.0
+        labelABV.text = "0 %"
+        labelVolume.text = "0"
+    }
+    var sliderVolumeround = Float()
+    var sliderABVround = Float()
+    
+    @IBOutlet weak var sliderABV: UISlider!
+    
+    @IBOutlet weak var labelABV: UILabel!
+    
+    @IBOutlet weak var sliderVolume: UISlider!
+    
+    @IBOutlet weak var labelVolume: UILabel!
+    
+    @IBAction func changeABV(sender: AnyObject)
+    {
+        sliderABVround = round(10 * sliderABV.value) / 10
+        labelABV.text = "\(sliderABVround) %"
     }
     
-    @IBOutlet weak var ABV: UITextField!
+    @IBAction func changeVolume(sender: AnyObject)
+    {
+        sliderVolumeround = round(10 * sliderVolume.value) / 10
+        labelVolume.text = "\(sliderVolumeround)"
+    }
     
-    @IBOutlet weak var Volume: UITextField!
-    
-    @IBAction func CustomDrinkPress(sender: AnyObject) {
-        //var custom: String = ABV.text
-        //var container: String = Volume.text
+    @IBAction func CustomDrinkPress(sender: AnyObject)
+    {
+        var alcoholConsumed: Float = sliderVolumeround * 28.3495231 * sliderABVround / 1400
         
-        var customABV: Float = (ABV.text as NSString).floatValue
-        var containerVol: Float = (Volume.text as NSString).floatValue
-        /*var doubleABV: Double = Double(customABV)
-        var cgabv = CGFloat(customABV)
-        var cgcontainer = CGFloat(containerVol)
-        var m = CGFloat(0)
-        var n = CGFloat(100)*/
-
+        var alertView: UIAlertView = UIAlertView()
+        alertView.title = "Success!"
+        alertView.message = "You have entered \(sliderVolumeround) ounces of a drink with \(sliderABVround)% ABV."
+        alertView.delegate = self
+        alertView.addButtonWithTitle("OK")
+        alertView.show()
         
-        //var customABV1 = custom.toInt()
-        //var containerVol1 = container.toInt()
-        var alcoholConsumed: Float = 0.0
-        var success = String()
-        //var containerint: Int = Int(containerVol)
-        //var ABVint: Int = Int(customABV)
-        
-        //let hundred: Float = 100.0
-        
-        //retract keyboard
-        ABV.resignFirstResponder()
-        Volume.resignFirstResponder()
-        
-        if (ABV.text == "")
+        // Add alcoholConsumed (number of Standard Drinks) to counter, and synchronize with NSUserDefaults
+        var defaults: NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        var oldCounter = defaults.floatForKey("COUNTER")
+        var newCounter = oldCounter + alcoholConsumed
+        defaults.setObject(newCounter, forKey: "COUNTER")
+        if (defaults.floatForKey("STARTTIME") == 0)
         {
-            // Alert if user did not enter an ABV
-            var alertView: UIAlertView = UIAlertView()
-            alertView.title = "Drink Failed!"
-            alertView.message = "Please enter an ABV."
-            alertView.delegate = self
-            alertView.addButtonWithTitle("OK")
-            alertView.show()
-        }
-        else if (Volume.text == "")
-        {
-            //Alert if user did not enter volume
-            var alertview: UIAlertView = UIAlertView()
-            alertview.title = "Drink Failed!"
-            alertview.message = "Please enter a volume of alcohol."
-            alertview.delegate = self
-            alertview.addButtonWithTitle("OK")
-            alertview.show()
-        }
-            
-        else if (0 >= containerVol || containerVol > 100)
-        {
-            //Alert if user input an invalid Volume
-            var alertview: UIAlertView = UIAlertView()
-            alertview.title = "Drink Failed!"
-            alertview.message = "Please enter a volume between 0 and 100."
-            alertview.delegate = self
-            alertview.addButtonWithTitle("OK")
-            alertview.show()
-        }
-        else if (0 >= customABV || customABV > 100)
-        {
-            //Alert if user input an invalid ABV
-            var alertview: UIAlertView = UIAlertView()
-            alertview.title = "Drink Failed!"
-            alertview.message = "Please enter an ABV between 0 and 100."
-            alertview.delegate = self
-            alertview.addButtonWithTitle("OK")
-            alertview.show()
-        }
-
-        
-        else
-        {
-            alcoholConsumed = containerVol * 28.3495231 * customABV/1400
-            
-            success = "You have entered a drink."
-            var alertView: UIAlertView = UIAlertView()
-            alertView.title = "Success!"
-            alertView.message = success
-            alertView.delegate = self
-            alertView.addButtonWithTitle("OK")
-            alertView.show()
-            
-            // Add alcoholConsumed (number of Standard Drinks) to counter, and synchronize with NSUserDefaults
-            var defaults: NSUserDefaults = NSUserDefaults.standardUserDefaults()
-            var oldCounter = defaults.floatForKey("COUNTER")
-            var newCounter = oldCounter + alcoholConsumed
-            defaults.setObject(newCounter, forKey: "COUNTER")
-            if (defaults.floatForKey("STARTTIME") == 0)
-            {
-                var startTime = NSDate.timeIntervalSinceReferenceDate()
-                defaults.setObject(startTime, forKey: "STARTTIME")
-                defaults.synchronize()
-            }
-            
-            
-            // Pop to root view controller ("counter" screen)
-            self.navigationController!.popToRootViewControllerAnimated(true)
+            var startTime = NSDate.timeIntervalSinceReferenceDate()
+            defaults.setObject(startTime, forKey: "STARTTIME")
+            defaults.synchronize()
         }
         
-        if (success != "You have entered a drink.")
-        {
-            var alertView: UIAlertView = UIAlertView()
-            alertView.title = "Error"
-            alertView.message = success
-            alertView.delegate = self
-            alertView.addButtonWithTitle("OK")
-            alertView.show()
-        }
-
         
-        
+        // Pop to root view controller ("counter" screen)
+        self.navigationController!.popToRootViewControllerAnimated(true)
     }
     
     override func didReceiveMemoryWarning() {
